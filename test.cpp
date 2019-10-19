4fc88434b1022f912e7afa1ce3ab2d76e47417f2
Ilya Ryzhkov s21357 <s21357@pjwstk.edu.pl>
2019-10-19 15:41:34 +0200

    Second example ready

--- test.cpp
+++ test.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <random>
 int main(){
-    //auto const n = 10;
-    //std::cout<<sizeof(n)<<"\n";
-    std::cout<<sizeof(std::string*)<<"\n";
+
+    auto const N = 10;
+    int an_array[N];
+    {
+        std::random_device rd;
+	std::uniform_int_distribution<int> generator(0,100);
+	for (auto i = 0; i < N; i++) {
+	    an_array[i] = generator(rd);
+	}
+    }
+    std::cout << "sizeof(an_array): "<< sizeof(an_array) <<"\n";
+    for (auto i = 0; i < N; i++) {
+        std::cout << an_array[i] << "\n";
+    }
+       
     return 0;
 }
