bcd46d9ed9cfef82ff80a0b5c44913f2649adab6
Ilya Ryzhkov s21357 <s21357@pjwstk.edu.pl>
2019-10-19 16:12:00 +0200

    Add bubble sorting

--- test.cpp
+++ test.cpp
@@ -11,6 +11,18 @@ int main(){
 	    an_array[i] = generator(rd);
 	}
     }
+    for (auto i = 0; i < N; i++) {
+        auto last = an_array[i];
+        auto index = i;
+        for (auto j = i; j < N; j++) {
+	    if (an_array[j] < last) {
+	        last = an_array[j];
+	        index = j;
+            }
+        }
+    an_array[index] = an_array[i];
+    an_array[i] = last;
+    }	
     std::cout << "sizeof(an_array): "<< sizeof(an_array) <<"\n";
     for (auto i = 0; i < N; i++) {
         std::cout << an_array[i] << "\n";
